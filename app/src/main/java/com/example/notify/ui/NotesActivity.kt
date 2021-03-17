1ed1bb5dcf7e52b424231cf0fd94df53ef71e117
noman <businessmail1997@gmail.com>
2021-03-17 16:03:21 +0500

    Added a log message

--- app/src/main/java/com/example/notify/ui/NotesActivity.kt
+++ app/src/main/java/com/example/notify/ui/NotesActivity.kt
@@ -1,6 +1,7 @@
 package com.example.notify.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.notify.R
 
@@ -8,5 +9,7 @@ class NotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
+
+        Log.d("hello", "onCreate: ")
     }
 }
